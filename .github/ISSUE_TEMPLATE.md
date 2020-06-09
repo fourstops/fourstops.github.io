dfe371585289d45eff85fa3adb08ab99b563730a
Four Stops <fourstops@gmail.com>
2020-06-09 10:41:24 -0400

    0.0.2

--- .github/ISSUE_TEMPLATE.md
+++
@@ -1,2 +0,0 @@
-
-If you're reporting an error, please try to provide detailed error messages and, if possible, instructions on how to replicate the issue. 
---
+++ pointclouds/Jarretsville/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=18 +ellps=WGS84 +datum=WGS84 +units=m",
+    "points": 16947520,
+    "boundingBox": {
+        "lx": 363955.489542,
+        "ly": 4369728.264076,
+        "lz": 53.552753,
+        "ux": 365030.712321,
+        "uy": 4370803.486854,
+        "uz": 1128.775531
+    },
+    "tightBoundingBox": {
+        "lx": 363999.250254,
+        "ly": 4369776.974788,
+        "lz": 60.462753,
+        "ux": 364452.339864,
+        "uy": 4370296.455821,
+        "uz": 156.044888
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 8.400178,
+    "scale": 0.010000,
+    "hierarchyStepSize": 5
+}
